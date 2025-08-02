cc4144c6c253a94c1b6ec27b53c9cfdd01a3621b
margarethdis <margarethdani2021@gmail.com>
2025-08-02 08:07:28 -0600

    set and maps test

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,23 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        var seen = new HashSet<string>();
+        var result = new List<string>();
+
+        foreach (string word in words)
+        {
+            string reversed = new string(word.Reverse().ToArray());
+
+            if (seen.Contains(reversed))
+            {
+                result.Add($"{reversed} & {word}");
+            }
+
+            seen.Add(word);
+        }
+
+        return result.ToArray();
+        
     }
 
     /// <summary>
